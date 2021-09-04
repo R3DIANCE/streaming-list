de499fd6112c09e9b11ce83f2138548fc44d173d
Nickwasused <34883496+Nickwasused@users.noreply.github.com>
2021-09-04 11:26:24 +0200

    Update time.js

--- src/js/time.js
+++ src/js/time.js
@@ -1,7 +1,7 @@
 export function isafternow(time) {
     let now = new Date();
     let timetocheck = new Date(time);
-    if (now.toTimeString() > timetocheck.toTimeString()) {
+    if (now > timetocheck) {
         return true
     } else {
         return false

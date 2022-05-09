ce14964194ec5323ad905d16bc94877f0c8b9d4f
Nickwasused <nickwasused.services@protonmail.com>
2022-05-09 18:39:46 +0200

    Update streamers.js

--- functions/api/streamers.js
+++ functions/api/streamers.js
@@ -1,6 +1,16 @@
 export async function onRequestGet({ params }) {
     const res = await fetch(`https://twitch-search-server.nickwasused.com/search?title=luckyv,lucky v`);
     const data = await res.json();
-    return new Response(data);
+    const info = JSON.stringify(data);
+    return new Response(info, {
+        headers: {
+          // add your origin here
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS",
+          "Content-Type": "application/json",
+          "Cache-Control": "max-age=120"
+        },
+        status: 200,
+    });
 }
   

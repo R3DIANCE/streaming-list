0162395107237966ef881f7365f539148685fa33
Nickwasused <nickwasused.services@protonmail.com>
2022-05-23 13:28:47 +0200

    fix response object

--- api/altv.js
+++ api/altv.js
@@ -4,14 +4,9 @@ export default async function handler(request, response) {
   const res = await fetch(`https://api.altv.mp/server/${process.env.ALTV_SERVER_ID}`);
   const data = await res.json();
   const info = JSON.stringify(data);
-  return new Response(info, {
-    headers: {
-      // add your origin here
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS",
-      "Content-Type": "application/json",
-      "Cache-Control": "max-age=120"
-    },
-    status: 200,
-  });
+  res.setHeader('Access-Control-Allow-Origin', process.env.VERCEL_URL);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS');
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Cache-Control', 's-maxage=60');
+  return response.status(200).json({ info });
 }
--- api/streamers.js
+++ api/streamers.js
@@ -4,13 +4,9 @@ export default async function handler(request, response) {
   const res = await fetch(process.env.SEARCH_SERVER);
   const data = await res.json();
   const info = JSON.stringify(data);
-  return new Response(info, {
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS",
-      "Content-Type": "application/json",
-      "Cache-Control": "max-age=120"
-    },
-    status: 200,
-  });
+  res.setHeader('Access-Control-Allow-Origin', process.env.VERCEL_URL);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS');
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Cache-Control', 's-maxage=120');
+  return response.status(200).json({ info });
 }

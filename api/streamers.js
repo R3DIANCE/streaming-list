686bbb7522d63522d83dc5a1166e2bddde088e06
Nickwasused <nickwasused.services@protonmail.com>
2022-05-23 13:20:17 +0200

    add serverless vercel functions

---
+++ api/streamers.js
@@ -0,0 +1,16 @@
+import fetch from 'node-fetch';
+
+export default async function handler(request, response) {
+  const res = await fetch(env.SEARCH_SERVER);
+  const data = await res.json();
+  const info = JSON.stringify(data);
+  return new Response(info, {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS",
+      "Content-Type": "application/json",
+      "Cache-Control": "max-age=120"
+    },
+    status: 200,
+  });
+}

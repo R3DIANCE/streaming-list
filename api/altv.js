0f98817da242a14957927b324a57164bbba695c0
Nickwasused <nickwasused.services@protonmail.com>
2022-05-23 13:38:07 +0200

    return data directly && try to change user agent

--- api/altv.js
+++ api/altv.js
@@ -1,12 +1,13 @@
 import fetch from 'node-fetch';
 
 export default async function handler(request, response) {
-  const res = await fetch(`https://api.altv.mp/server/${process.env.ALTV_SERVER_ID}`);
+  const res = await fetch(`https://api.altv.mp/server/${process.env.ALTV_SERVER_ID}`, {
+    headers: { 'User-Agent': 'Vercel' },
+  });
   const data = await res.json();
-  const info = JSON.stringify(data);
   response.setHeader('Access-Control-Allow-Origin', process.env.VERCEL_URL);
   response.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS');
   response.setHeader('Content-Type', 'application/json');
   response.setHeader('Cache-Control', 's-maxage=60');
-  return response.status(200).json({ info });
+  return response.status(200).json(data);
 }
--- api/streamers.js
+++ api/streamers.js
@@ -1,12 +1,13 @@
 import fetch from 'node-fetch';
 
 export default async function handler(request, response) {
-  const res = await fetch(process.env.SEARCH_SERVER);
+  const res = await fetch(process.env.SEARCH_SERVER, {
+    headers: { 'User-Agent': 'Vercel' },
+  });
   const data = await res.json();
-  const info = JSON.stringify(data);
   response.setHeader('Access-Control-Allow-Origin', process.env.VERCEL_URL);
   response.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS');
   response.setHeader('Content-Type', 'application/json');
   response.setHeader('Cache-Control', 's-maxage=120');
-  return response.status(200).json({ info });
+  return response.status(200).json(data);
 }

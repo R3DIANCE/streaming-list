010460aebea9fda0efb01da838116315a8566bac
Nickwasused <nickwasused.services@protonmail.com>
2022-05-24 11:58:57 +0200

    change service worker register

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import { registerSW } from 'virtual:pwa-register'
 
-registerSW()
+window.addEventListener('load', () => {
+    registerSW({ immediate: true })
+})
 
 const app = createApp(App);
 

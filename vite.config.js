0899c79f98e39c74dbcfa087f8af85d4ee0cd8a4
Nickwasused <github.lix1s@simplelogin.co>
2022-11-29 14:34:42 +0100

    build sourcemap only on preview and development builds

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ import vueI18n from "@intlify/vite-plugin-vue-i18n"
 import path from "path"
 
 const env = loadEnv("production", process.cwd(), '')
-
-console.log(env.VERCEL_ENV == "production");
+let build_source_map = true;
+if (env.VERCEL_ENV == "production") { build_source_map = false; }
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,7 +20,7 @@ export default defineConfig({
             registerType: "autoUpdate",
             workbox: {
                 cleanupOutdatedCaches: false,
-                sourcemap: true,
+                sourcemap: build_source_map,
                 globPatterns: [
                     "**/*.{woff2,ttf,eot,woff,svg,css,js,jpg,ico,png,webp,html,json,txt}",
                 ],

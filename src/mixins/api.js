91e1ad7cf5f161c1cd8400b75093630b3d620230
Nickwasused <github.lix1s@simplelogin.co>
2022-12-06 15:32:56 +0100

    optimize localstorage?

--- src/mixins/api.js
+++ src/mixins/api.js
@@ -1,32 +1,37 @@
 const api = {
-    // https://pieroxy.net/blog/pages/lz-string/demo.html
     async fetch_or_cache(url, key, minuets = 2) {
-        let now = new Date()
-        let api_data
+        let now = new Date();
+        let api_data;
+
+        const local_item = localStorage.getItem(key);
 
         if (
-            localStorage[key] == undefined ||
-            localStorage[`${key}:invalidate`] == undefined ||
+            local_item == undefined ||
+            localStorage.getItem(`${key}:invalidate`) == undefined ||
             now > new Date(localStorage[`${key}:invalidate`])
         ) {
             // fetch new data
-            console.debug(`fetching new data for: ${key}`)
+            console.debug(`fetching new data for: ${key}`);
             try {
-                const response = await fetch(url)
-                api_data = await response.json()
+                const response = await fetch(url);
+                api_data = await response.json();
 
                 // set data to localstorage and set invaliddate
-                now.setMinutes(now.getMinutes() + minuets)
-                localStorage[`${key}:invalidate`] = now
-                localStorage[key] = JSON.stringify(api_data)
+                now.setMinutes(now.getMinutes() + minuets);
+                localStorage.setItem(`${key}:invalidate`, now);
+                localStorage.setItem(key, JSON.stringify(api_data));
             } catch (error) {
                 console.warn(`error while fetching resource: ${error}`)
-                return {}
+                if (local_item == undefined) {
+                    return {}
+                } else {
+                    return JSON.parse(local_item);
+                }
             }
         } else {
             // load data from cache
-            console.debug(`using cached data for: ${key}`)
-            api_data = JSON.parse(localStorage[key])
+            console.debug(`using cached data for: ${key}`);
+            api_data = JSON.parse(local_item);
         }
 
         return api_data

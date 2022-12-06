f922344e3b0a1c68485849d06d7863fcbbddc2f8
Nickwasused <github.lix1s@simplelogin.co>
2022-12-06 13:19:54 +0100

    Revert "try to optimize fetching streamers and tags"
    This reverts commit ecee54672bf76520fc545644f3f146fc77a2b074.

--- src/components/StreamerList.vue
+++ src/components/StreamerList.vue
@@ -109,32 +109,6 @@ export default {
         // alphabetically_az, alphabetically_za, viewer_high, viewer_low, shuffle
         const filter = ref("viewer_high");
 
-        async function get_streamers() {
-            let api_data = await api.fetch_or_cache(
-                import.meta.env.VERCEL_ENV == "production"
-                    ? "/api/streamers"
-                    : import.meta.env.VITE_SEARCH_SERVER,
-                "streamers"
-            )
-
-            if (api_data == {}) {
-                api_data = []
-            }
-
-            this.streamers = api_data
-        }
-
-        async function get_tags() {
-            // cache for 7 days
-            const api_data = await api.fetch_or_cache(
-                "https://raw.githubusercontent.com/Nickwasused/twitch-tag-list/gh-pages/tags.min.json",
-                "tags",
-                10080
-            )
-
-            this.tags = api_data
-        }
-
         return {
             streamers,
             views,
@@ -145,8 +119,6 @@ export default {
             show_filters,
             small_device,
             filter,
-            get_streamers,
-            get_tags,
             locale, t
         };
     },
@@ -250,6 +222,44 @@ export default {
             this.small_device = width < 742
             this.show_filters = !this.small_device
         },
+        remove_duplicate_streamers(array) {
+            // our api server is not always returning correct data!
+            if (array.length == 0) { return [] }
+            const tmp_ids = [];
+            let new_data = [];
+            array.forEach(entry => {
+                if (!tmp_ids.includes(entry["id"])) {
+                    tmp_ids.push(entry["id"]);
+                    new_data.push(entry);
+                }
+            });
+
+            return new_data;
+        },
+        async get_streamers() {
+            let api_data = await api.fetch_or_cache(
+                import.meta.env.VERCEL_ENV == "production"
+                    ? "/api/streamers"
+                    : import.meta.env.VITE_SEARCH_SERVER,
+                "streamers"
+            )
+
+            if (api_data == {}) {
+                api_data = []
+            }
+
+            this.streamers = this.remove_duplicate_streamers(api_data)
+        },
+        async get_tags() {
+            // cache for 7 days
+            const api_data = await api.fetch_or_cache(
+                "https://raw.githubusercontent.com/Nickwasused/twitch-tag-list/gh-pages/tags.min.json",
+                "tags",
+                10080
+            )
+
+            this.tags = api_data
+        },
         set_total_views(viewers) {
             this.$emit("total-viewers", viewers)
         },
@@ -316,14 +326,12 @@ export default {
         },
     },
     mounted: function () {
-        this.get_streamers();
-        this.get_tags();
+        window.addEventListener("resize", this.window_resize)
         if (this.timer == null) {
             this.timer = setInterval(() => {
                 this.updatedata()
             }, 300000)
         }
-        window.addEventListener("resize", this.window_resize)
     },
     unmounted() {
         clearInterval(this.timer)

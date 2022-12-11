3ce499ccca4fc44139a9b3e5f39a8df7a32beaa1
Nickwasused <github.lix1s@simplelogin.co>
2022-12-11 14:49:04 +0100

    Update LazyLoadDirective.js

--- src/directives/LazyLoadDirective.js
+++ src/directives/LazyLoadDirective.js
@@ -5,7 +5,7 @@ const options = {
 };
 
 export default {
-    mounted: function(el) {
+    created: function(el) {
         const observer = new IntersectionObserver((entries, observer) => {
             // we only expect one observer as we apply it directly to the image tag!
             if (entries[0].isIntersecting) {
@@ -16,7 +16,7 @@ export default {
         observer.observe(el);
     },
     updated: function(el) {
-        const observer = new IntersectionObserver((entries, observer) => {
+        const observer = new IntersectionObserver(entries => {
             if (entries[0].isIntersecting) {
                 entries[0].target.srcset = entries[0].target.dataset.srcset;
                 observer.unobserve(el);

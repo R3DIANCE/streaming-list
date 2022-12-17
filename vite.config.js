57e6000285cadabf810122f3e8b21b9ac74f2b01
Nickwasused <github.lix1s@simplelogin.co>
2022-12-17 17:19:54 +0100

    add maskable icon

--- vite.config.js
+++ vite.config.js
@@ -84,6 +84,11 @@ export default defineConfig({
                         src: "img/icons/android-chrome-512x512.png",
                         sizes: "512x512",
                         type: "image/png",
+                    },
+                    {
+                        src: "img/icons/android-chrome-512x512-maskable.png",
+                        sizes: "512x512",
+                        type: "image/png",
                         purpose: "any maskable",
                     },
                 ],

1e1b3a8b16fe4f487ef42b480828aabffb3560fc
nickwasused <github.lix1s@simplelogin.co>
2023-06-20 13:20:09 +0200

    Revert "change to sentry.io"
    This reverts commit 4dc67b6c5ea49afdfd36ab31bae3e8728428b5d5.

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,12 @@ import App from "./App.vue"
 import LazyLoadDirective from "./directives/LazyLoadDirective.js"
 import { registerSW } from "virtual:pwa-register"
 import * as VueI18n from "vue-i18n"
-import * as Sentry from "@sentry/vue";
 
 window.addEventListener("load", () => {
     registerSW({ immediate: true })
 })
 
 const app = createApp(App)
-
-Sentry.init({
-    app,
-    dsn: "https://e2caf30e811348dcb0a10a8f0b9580c5@o4505391383838720.ingest.sentry.io/4505391386001408",
-});
 
 const client = createClient({
     url: "https://tts-de-gta5.nickwasused.com/graphql",
